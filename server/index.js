25b44714ac484cfbacf7715466955fbe86f4f018
Felipe Chernicharo <string7dev@gmail.com>
2022-12-01 01:49:02 -0500

    server: users CRUD ok

--- server/index.js
+++ server/index.js
@@ -1,91 +1,5 @@
-import express, { json } from "express";
-import cors from "cors";
-
-const DB = {
-	user: {
-		id: 9876,
-		name: "Felipe",
-		active: true,
-		skills: [
-			{ name: "JS", pro: true },
-			{ name: "redux", pro: false },
-		],
-	},
-	preferences: {
-		order: "aToZ",
-		displayDensity: "comfortable",
-	},
-	chats: {
-		1: {
-			id: 1,
-			name: "only me",
-			participants: [{ id: 9876, name: "Felipe" }],
-		},
-		2: {
-			id: 2,
-			name: "Mari",
-			participants: [
-				{ id: 9876, name: "Felipe" },
-				{ id: 1234, name: "Mari" },
-			],
-		},
-		3: {
-			id: 3,
-			name: "Samba",
-			participants: [
-				{ id: 9876, name: "Felipe" },
-				{ id: 121, name: "Rafael" },
-				{ id: 1234, name: "Mari" },
-				{ id: 122, name: "Fernando" },
-				{ id: 555, name: "Rebeca" },
-			],
-		},
-	},
-	messages: {
-		0: {
-			id: 0,
-			chatId: 1,
-			body: "Some Code",
-			timestamp: 1_669_866_573_422,
-			sender: { id: 9876, name: "Felipe" },
-		},
-		1: {
-			id: 1,
-			chatId: 2,
-			body: "Oi Amor!",
-			timestamp: 1669866583422,
-			sender: { id: 9876, name: "Felipe" },
-		},
-		2: {
-			id: 2,
-			chatId: 3,
-			body: "Bora agitar um samba essa sexta?",
-			timestamp: 1669866593422,
-			sender: { id: 121, name: "Rafael" },
-		},
-		3: {
-			id: 3,
-			chatId: 2,
-			body: "Pilho!",
-			timestamp: 1669866603422,
-			sender: { id: 9876, name: "Felipe" },
-		},
-		4: {
-			id: 4,
-			chatId: 2,
-			body: "Vou levar o pandeiro!",
-			timestamp: 1669866613422,
-			sender: { id: 122, name: "Fernando" },
-		},
-		5: {
-			id: 5,
-			chatId: 2,
-			body: "Oi Bichinho! ❤️",
-			timestamp: 1669866623422,
-			sender: { id: 1234, name: "Mari" },
-		},
-	},
-};
+import express, { json } from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
@@ -114,44 +28,194 @@ function logger(req, res, next) {
 	next();
 }
 
-let currMsgId = DB.messages;
-app.get("/", (req, res) => {
-	res.status(200).send({ mesage: "hello" });
+const DB = {
+	users: {
+		9876: {
+			id: 9876,
+			name: 'Felipe',
+			active: true,
+			skills: [
+				{ name: 'JS', pro: true },
+				{ name: 'redux', pro: false },
+				{ name: 'guitar', pro: true },
+			],
+		},
+		121: {
+			id: 121,
+			name: 'Rafael',
+			active: true,
+			skills: [
+				{ name: 'Cavaco', pro: false },
+				{ name: 'Python', pro: true },
+				{ name: 'R', pro: true },
+			],
+		},
+		122: {
+			id: 122,
+			name: 'Fernando',
+			active: true,
+			skills: [
+				{ name: 'Python', pro: true },
+				{ name: 'markdown', pro: false },
+				{ name: 'PowerBI', pro: true },
+				{ name: 'volei', pro: false },
+			],
+		},
+		1234: {
+			id: 1234,
+			name: 'Mari',
+			active: true,
+			skills: [
+				{ name: 'Excel', pro: true },
+				{ name: 'Powerpoint', pro: true },
+				{ name: 'volei', pro: false },
+			],
+		},
+		555: {
+			id: 1234,
+			name: 'Rebeca',
+			active: true,
+			skills: [
+				{ name: 'Python', pro: true },
+				{ name: 'Excel', pro: true },
+			],
+		},
+	},
+	preferences: {
+		order: 'aToZ',
+		displayDensity: 'comfortable',
+	},
+	chats: {
+		1: {
+			id: 1,
+			name: 'only me',
+			participants: [{ id: 9876, name: 'Felipe' }],
+		},
+		2: {
+			id: 2,
+			name: 'Mari',
+			participants: [
+				{ id: 9876, name: 'Felipe' },
+				{ id: 1234, name: 'Mari' },
+			],
+		},
+		3: {
+			id: 3,
+			name: 'Samba',
+			participants: [
+				{ id: 9876, name: 'Felipe' },
+				{ id: 121, name: 'Rafael' },
+				{ id: 1234, name: 'Mari' },
+				{ id: 122, name: 'Fernando' },
+				{ id: 555, name: 'Rebeca' },
+			],
+		},
+	},
+	messages: {
+		0: {
+			id: 0,
+			chatId: 1,
+			body: 'Some Code',
+			timestamp: 1_669_866_573_422,
+			sender: { id: 9876, name: 'Felipe' },
+		},
+		1: {
+			id: 1,
+			chatId: 2,
+			body: 'Oi Amor!',
+			timestamp: 1669866583422,
+			sender: { id: 9876, name: 'Felipe' },
+		},
+		2: {
+			id: 2,
+			chatId: 3,
+			body: 'Bora agitar um samba essa sexta?',
+			timestamp: 1669866593422,
+			sender: { id: 121, name: 'Rafael' },
+		},
+		3: {
+			id: 3,
+			chatId: 2,
+			body: 'Pilho!',
+			timestamp: 1669866603422,
+			sender: { id: 9876, name: 'Felipe' },
+		},
+		4: {
+			id: 4,
+			chatId: 2,
+			body: 'Vou levar o pandeiro!',
+			timestamp: 1669866613422,
+			sender: { id: 122, name: 'Fernando' },
+		},
+		5: {
+			id: 5,
+			chatId: 2,
+			body: 'Oi Bichinho! ❤️',
+			timestamp: 1669866623422,
+			sender: { id: 1234, name: 'Mari' },
+		},
+	},
+};
+
+let currMsgId = 6;
+let currUserId = 9877;
+
+// ******* USERS ********
+app.get('/users', (req, res) => {
+	res.status(200).send(DB.users);
 });
 
-app.post("/", (req, res) => {
-	console.log(req.body);
-
-	res.status(200).send({ message: "ok" });
+app.get('/users/:id', (req, res) => {
+	res.status(200).send(DB.users[req.params.id]);
 });
 
-app.get("/user", (req, res) => {
-	res.status(200).send(DB.user);
+app.post('/users', (req, res) => {
+	const id = currUserId++;
+
+	const newUser = {
+		id,
+		name: req.body.name,
+		active: true,
+		skills: [],
+	};
+	DB.users[id] = newUser;
+	res.status(200).send(newUser);
 });
 
-app.get("/chats", (req, res) => {
+app.patch('/users/:id', (req, res) => {
+	const { id } = req.params;
+	const updatedUser = { ...DB.users[id], ...req.body };
+	DB.users[id] = { ...updatedUser };
+
+	res.status(200).send({
+		user: DB.users[id],
+	});
+});
+
+app.delete('/users/:id', (req, res) => {
+	const deletedUser = DB.users[req.params.id];
+	delete DB.users[req.params.id];
+
+	res.status(200).send({ deletedUser });
+});
+
+// ******* CHATS ********
+app.get('/chats', (req, res) => {
 	res.status(200).send(DB.chats);
 });
 
-app.get("/messages", (req, res) => {
+// ******* MESSAGES ********
+app.get('/messages', (req, res) => {
 	// res.status(200).send(DB.messages);
 	const messages = toArray(DB.messages);
 
 	res.status(200).send({ messages });
 });
 
-app.patch("/user", (req, res) => {
-	const { body } = req;
-	console.log(body);
-
-	DB.user = { ...DB.user, ...body };
-	res.status(200).send({ message: "ok" });
-});
-
-app.post("/message", (req, res) => {
+app.post('/message', (req, res) => {
 	console.log(req.body);
 });
 
 app.listen(8000, () => {
-	console.log("listening on port 8000");
+	console.log('listening on port 8000');
 });
